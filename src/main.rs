360cdf6694a72c8374af92c050b56e8cad49ee64
Benjamin Fox <benjaminf@intergen.co.nz>
2015-12-14 15:02:03 +1300

    Basic processor

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate parser_ast;
 use std::env::args;
 use std::fs::File;
 use std::io::{ BufRead, BufReader };
+use std::collections::HashMap;
 
 use parser_ast::ast::*;
 use parser_ast::parser::parse_instruction;
@@ -15,14 +16,14 @@ fn open_file() -> File {
     File::open(filename).expect("Error opening input")
 }
 
-fn read_instructions(file: File) -> Vec<Result<Instruction, String>> {
+fn read_instructions(file: File) -> Vec<Instruction> {
     BufReader::new(file)
         .lines()
         .map(|line| line.expect("Error reading file"))
         .map(|line| {
             match parse_instruction(&line) {
-                Ok(val) => Ok(val),
-                Err(e)  => Err(format!("Error parsing line `{}`: {}", line, e))
+                Ok(val) => val,
+                Err(e)  => panic!("Error parsing line `{}`: {}", line, e)
             }
         })
         .collect()
@@ -32,10 +33,44 @@ fn main() {
     
     let instructions = read_instructions(open_file());
     
+    // Map of wire labels to signals
+    let mut map = HashMap::new();
+    
     for inst in instructions {
-        match inst {
-            Ok(inst) => println!("{:?}", inst),
-            Err(msg) => println!("Error: {}", msg) 
-        }
+        
+        let signal: Signal = {
+            match inst.expr {
+                Expr::Input(src) => resolve(&map, src),
+                Expr::Gate2(gate, a, b) => {
+                    let a = resolve(&map, a);
+                    let b = resolve(&map, b);
+                    match gate {
+                        Gate2::AND    => a & b, 
+                        Gate2::OR     => a | b,
+                        Gate2::RSHIFT => a >> b,
+                        Gate2::LSHIFT => a << b
+                    }
+                },
+                Expr::Gate1(gate, a) => {
+                    let a = resolve(&map, a);
+                    match gate {
+                        Gate1::NOT => !a
+                    }
+                }
+            }
+        };
+        
+        map.insert(inst.target, signal);
+    }
+    
+    for (key, value) in map.iter() {
+        println!("{}: {}", key, value);
+    }
+}
+
+fn resolve(map: &HashMap<Label, Signal>, source: Source) -> Signal { 
+    match source {
+        Source::Const(s) => s,
+        Source::Wire(label) => *map.get(&label).unwrap_or_else(|| panic!("No signal value for {}", label))
     }
 }
