12b405219772bc55a54bf26ea9036e9e624950bd
Benjamin Fox <benjaminf@intergen.co.nz>
2015-12-14 17:12:04 +1300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::env::args;
+use std::fs::File;
+
+fn open_file() -> File {
+    let filename = args().skip(1).next().expect("usage: day8 {input filename}");
+    File::open(filename).expect("Error opening input")
+}
+
+fn main() {
+    unimplemented!()
+}
