b59208632208595d9cbb59d9e701b3523f345d7a
Benjamin Fox <deadalus.ai@gmail.com>
2015-12-18 00:14:06 +1300

    Done 1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(iter_arith)]
+
 extern crate serde_json;
 
 use std::env::args;
@@ -14,14 +16,34 @@ fn open_file() -> File {
 
 fn read_input(mut file: File) -> Value {
     let mut json = String::new();
-    file.read_to_string(&mut json);
-
+    file.read_to_string(&mut json).expect("Error reading file");
     serde_json::from_str(&json).expect("Error parsing input")
+}
+
+fn recursive_sum_numbers(json: &Value) -> i64 {
+    match json {
+        &Value::Object(ref map) => {
+            map.iter()
+               .map(|(_, val)| recursive_sum_numbers(val))
+               .sum()
+        },
+        &Value::Array(ref arr) => {
+            arr.iter()
+               .map(|val| recursive_sum_numbers(val))
+               .sum()
+        },
+        &Value::U64(ref num) => *num as i64,
+        &Value::F64(ref num) => *num as i64,
+        &Value::I64(ref num) => *num,
+        _                    => 0,
+    }
 }
 
 fn main() {
 
     let json = read_input(open_file());
 
-    println!("{:?}", json);
+    let result = recursive_sum_numbers(&json);
+
+    println!("Sum of all numbers in the input: {:?}", result);
 }
