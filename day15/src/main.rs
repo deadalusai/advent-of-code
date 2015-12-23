42c752f6609bdbfca455fb81fd46d932f6b02e45
Benjamin Fox <deadalus.ai@gmail.com>
2015-12-24 10:49:35 +1300

    Done 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -28,20 +28,13 @@ fn parse_ingredient(s: &str) -> Result<Ingredient, String> {
     match re.captures(s) {
         None => Err("Unable to parse ingredient".into()),
         Some(caps) => {
-            let name       = caps.at(1).unwrap().into();
-            let capacity   = caps.at(2).unwrap().parse().unwrap();
-            let durability = caps.at(3).unwrap().parse().unwrap();
-            let flavor     = caps.at(4).unwrap().parse().unwrap();
-            let texture    = caps.at(5).unwrap().parse().unwrap();
-            let calories   = caps.at(6).unwrap().parse().unwrap();
-
             Ok(Ingredient {
-                name: name,
-                capacity: capacity,
-                durability: durability,
-                flavor: texture,
-                texture: flavor,
-                calories: calories,
+                name:       caps.at(1).unwrap().into(),
+                capacity:   caps.at(2).unwrap().parse().unwrap(),
+                durability: caps.at(3).unwrap().parse().unwrap(),
+                flavor:     caps.at(4).unwrap().parse().unwrap(),
+                texture:    caps.at(5).unwrap().parse().unwrap(),
+                calories:   caps.at(6).unwrap().parse().unwrap(),
             })
         }
     }
@@ -65,9 +58,14 @@ fn main() {
 
     // Calculate best ingredient mix by recursive brute-force solution
 
-    let best_mix = find_best(&ingredients, 100, Mix::empty());
+    let best_mix = find_best(&ingredients, 100, Mix::empty(), None);
 
-    println!("Best mix: {:?} ({})", &best_mix, best_mix.score());
+    println!("Best mix: {:?} ({})", &best_mix, best_mix.score(None));
+
+    let calorie_limit = Some(500);
+    let best_mix = find_best(&ingredients, 100, Mix::empty(), calorie_limit);
+
+    println!("Best mix with calorie limit: {:?} ({})", &best_mix, best_mix.score(calorie_limit));
 }
 
 #[derive(Debug)]
@@ -76,6 +74,7 @@ struct Mix {
     durability: i32,
     flavor: i32,
     texture: i32,
+    calories: i32
 }
 
 impl Mix {
@@ -85,20 +84,26 @@ impl Mix {
             durability: 0,
             flavor: 0,
             texture: 0,
+            calories: 0
         }
     }
 
-    fn is_poor_mix(&self) -> bool {
+    fn is_poor_mix(&self, calorie_requirement: Option<i32>) -> bool {
+        if let Some(required) = calorie_requirement {
+            if self.calories != required {
+                return true;
+            }
+        }
         // Operands swapped because < confuses my syntax highlighting
         1 > self.capacity || 1 > self.durability || 1 > self.flavor || 1 > self.texture
     }
 
-    fn score(&self) -> i32 {
-        if self.is_poor_mix() { 0 } else { self.capacity * self.durability * self.flavor * self.texture }
+    fn score(&self, calorie_requirement: Option<i32>) -> i32 {
+        if self.is_poor_mix(calorie_requirement) { 0 } else { self.capacity * self.durability * self.flavor * self.texture }
     }
 }
 
-fn find_best(ingredients: &[Ingredient], max_tsp: i32, input_mix: Mix) -> Mix {
+fn find_best(ingredients: &[Ingredient], max_tsp: i32, input_mix: Mix, calorie_requirement: Option<i32>) -> Mix {
 
     assert!(ingredients.len() >= 1);
     assert!(max_tsp >= 1);
@@ -110,7 +115,8 @@ fn find_best(ingredients: &[Ingredient], max_tsp: i32, input_mix: Mix) -> Mix {
             capacity:   input_mix.capacity   + (ing.capacity   * max_tsp),
             durability: input_mix.durability + (ing.durability * max_tsp),
             flavor:     input_mix.flavor     + (ing.flavor     * max_tsp),
-            texture:    input_mix.texture    + (ing.texture    * max_tsp)
+            texture:    input_mix.texture    + (ing.texture    * max_tsp),
+            calories:   input_mix.calories   + (ing.calories   * max_tsp)
         };
     }
 
@@ -122,16 +128,17 @@ fn find_best(ingredients: &[Ingredient], max_tsp: i32, input_mix: Mix) -> Mix {
             capacity:   input_mix.capacity   + (ing.capacity   * tsp),
             durability: input_mix.durability + (ing.durability * tsp),
             flavor:     input_mix.flavor     + (ing.flavor     * tsp),
-            texture:    input_mix.texture    + (ing.texture    * tsp)
+            texture:    input_mix.texture    + (ing.texture    * tsp),
+            calories:   input_mix.calories   + (ing.calories   * tsp)
         };
 
         let mix =
             if max_tsp - tsp <= 0 { mix }
-            else                  { find_best(&ingredients[1..], max_tsp - tsp, mix) };
+            else                  { find_best(&ingredients[1..], max_tsp - tsp, mix, calorie_requirement) };
 
         //println!("{}x{} => {} {:?}", tsp, max_tsp - tsp, mix.score(), mix);
 
-        if best_mix.score() < mix.score() {
+        if best_mix.score(calorie_requirement) < mix.score(calorie_requirement) {
             best_mix = mix;
         }
     }
