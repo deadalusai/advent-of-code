430b7f1eb8e6f317984bb6a9b3dae9102bfb3fdf
Benjamin Fox <deadalus.ai@gmail.com>
2015-12-23 20:16:14 +1300

    Regex solves a problem?

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,9 +1,13 @@
+extern crate regex;
+
 use std::env::args;
 use std::fs::File;
 
 use std::io::{ BufRead, BufReader };
 
 use std::collections::{ HashSet, HashMap };
+
+use regex::Regex;
 
 fn open_file() -> File {
     let filename = args().skip(1).next().expect("usage: day13 {input filename}");
@@ -23,68 +27,33 @@ struct Instruction {
     change: Change,
 }
 
-macro_rules! part {
-    ( $e:expr, $message:expr ) => {
-        match $e.next() {
-            Some(s) => s,
-            None => return Err(format!("Expected {}", $message))
-        }
-    }
-}
-
-macro_rules! expect {
-    ( $e:expr, $expected:expr ) => {
-        match $e.next() {
-            Some(s) if s != $expected => return Err(format!("Expected '{}'", $expected)),
-            None                      => return Err(format!("Expected '{}'", $expected)),
-            _                         => ()
-        }
-    }
-}
-
 fn parse_instruction(s: &str) -> Result<Instruction, String> {
-    
-    let mut parts = s.split(' ');
-    
-    let person = part!(parts, "person name");
-    
-    expect!(parts, "would");
-    
-    let change = part!(parts, "change type");
-    let change_amount = part!(parts, "change amount");
-    
-    let change_amount = match change_amount.parse() {
-        Ok(n) => n,
-        Err(e) => return Err(format!("Error parsing change amount: {}", e))
-    };
-    
-    let change = match change {
-        "gain" => Change::Gain(change_amount),
-        "lose" => Change::Lose(change_amount),
-        other  => return Err(format!("Invalid change amount: {}", other))
-    };
-    
-    expect!(parts, "happiness");
-    expect!(parts, "units");
-    expect!(parts, "by");
-    expect!(parts, "sitting");
-    expect!(parts, "next");
-    expect!(parts, "to");
-    
-    let neighbour = part!(parts, "person name");
-    
-    match neighbour.chars().rev().next() {
-        Some('.') => (),
-        _         => return Err("Expected .".into())       
+
+    let pattern = r"^([A-Za-z]+) would (gain|lose) (\d+) happiness units by sitting next to ([A-Za-z]+).$";
+    let re = Regex::new(pattern).unwrap();
+
+    match re.captures(s) {
+        None       => Err("Instruction does not match pattern".into()),
+        Some(caps) => {
+
+            let person    = caps.at(1).unwrap().into();
+            let change    = caps.at(2).unwrap();
+            let units     = caps.at(3).unwrap().parse().unwrap();
+            let neighbour = caps.at(4).unwrap().into();
+
+            let change = match change {
+                "gain" => Change::Gain(units),
+                "lose" => Change::Lose(units),
+                 _     => unreachable!()
+            };
+
+            Ok(Instruction {
+                person: person,
+                neighbour: neighbour,
+                change: change
+            })
+        }
     }
-    
-    let neighbour = &neighbour[..neighbour.len() - 1];
-    
-    if parts.next().is_some() {
-        return Err("Expected end of input".into());
-    }
-    
-    Ok(Instruction { person: person.into(), change: change, neighbour: neighbour.into() })
 }
 
 fn read_input(file: File) -> Vec<Instruction> {
@@ -96,34 +65,34 @@ fn read_input(file: File) -> Vec<Instruction> {
 }
 
 fn main() {
-    
+
     let instructions = read_input(open_file());
-    
+
     let mut people = HashSet::new();
     let mut relationships = HashMap::new();
-    
+
     for inst in instructions {
         people.insert(inst.person.clone());
         relationships.insert((inst.person, inst.neighbour), inst.change);
     }
-    
+
     people.insert("My own good self".into());
 
     let people: Vec<String> = people.into_iter().collect();
-    
+
     let mut happiness_delta_max = 0;
-    
+
     for p in permute(people) {
-        
+
         let mut happiness_delta = 0;
-    
+
         for (left, right) in pairs(&p) {
-            
+
             let keys = [
-                (left.clone(), right.clone()), 
+                (left.clone(), right.clone()),
                 (right.clone(), left.clone())
             ];
-            
+
             for key in keys.iter() {
                 match relationships.get(key) {
                     Some(&Change::Gain(ref amt)) => { happiness_delta += *amt; },
@@ -132,12 +101,12 @@ fn main() {
                 }
             }
         }
-        
+
         if happiness_delta > happiness_delta_max {
             happiness_delta_max = happiness_delta;
         }
     }
-    
+
     println!("Biggest happiness change: {}", happiness_delta_max);
 }
 
@@ -154,16 +123,16 @@ struct Pairs<'a, T: 'a> {
 
 impl <'a, T> Iterator for Pairs<'a, T> {
     type Item = (&'a T, &'a T);
-    
+
     fn next(&mut self) -> Option<Self::Item> {
-        
+
         let len = self.source.len();
         let next = match self.i {
             i if i == len - 1 => Some((&self.source[i], &self.source[0])),
             i if i >= len     => None,
             i                 => Some((&self.source[i], &self.source[i + 1]))
         };
-        
+
         self.i += 1;
         next
     }
@@ -180,9 +149,9 @@ struct Permute<T> {
 
 impl <T: Clone> Iterator for Permute<T> {
     type Item = Vec<T>;
-    
+
     fn next(&mut self) -> Option<Self::Item> {
-        
+
         // Simple case - source of one elements
         if self.source.len() <= 1 {
             let result = match self.index {
@@ -192,7 +161,7 @@ impl <T: Clone> Iterator for Permute<T> {
             self.index += 1;
             return result;
         }
-        
+
         // Simple case - source of two elements
         if self.source.len() == 2 {
             let result = match self.index {
@@ -203,23 +172,23 @@ impl <T: Clone> Iterator for Permute<T> {
             self.index += 1;
             return result;
         }
-        
+
         loop {
             // Done iterating?
             if self.index >= self.source.len() {
                 return None;
             }
-            
+
             if self.sub.is_none() {
                 let source = self.source.iter()
                                  .enumerate()
                                  .filter(|&(i, _)| i != self.index)
                                  .map(|(_, v)| v.clone())
                                  .collect();
-                
+
                 self.sub = Some(Box::new(permute(source)));
             }
-            
+
             let next = match self.sub.as_mut().unwrap().next() {
                 None => None,
                 Some(mut v) => {
@@ -227,7 +196,7 @@ impl <T: Clone> Iterator for Permute<T> {
                     Some(v)
                 }
             };
-            
+
             if next.is_none() {
                 //Reached the end of the sub iterator!
                 self.sub = None;
