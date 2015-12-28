ee9395a9d11a5a3afda2ba1995f73d252b632270
Benjamin Fox <deadalus.ai@gmail.com>
2015-12-28 20:28:21 +1300

    Done 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -45,14 +45,37 @@ fn main() {
     // Simulating a terminal world
     world.set_neighbours(terminal_neighbours);
 
+    // Part 2 - Keep the four corners turned on
+
     for _ in 0 .. iterations {
+        reset_corners(&mut world);
         world.step_mut();
+        reset_corners(&mut world);
     }
 
     let live_cell_count: usize =
-        world.iter_rows()
-             .map(|row| row.iter().filter(|c| c.is_live()).count())
+        world.grid()
+             .iter_rows()
+             .map(|row| {
+                 row.iter()
+                    .filter(|c| c.is_live())
+                    .count()
+             })
              .sum();
 
     println!("Lights on after {} iterations: {}", iterations, live_cell_count);
+}
+
+fn reset_corners(world: &mut World) {
+    // Find the x, y coordinates of the bottom right corner
+    let x_last = world.width() - 1;
+    let y_last = world.height() - 1;
+
+    let g = world.grid_mut();
+
+    // Resurrect the four corner cells
+    g.set_cell(0,      0,      Cell::Live);
+    g.set_cell(x_last, 0,      Cell::Live);
+    g.set_cell(0,      y_last, Cell::Live);
+    g.set_cell(x_last, y_last, Cell::Live);
 }
