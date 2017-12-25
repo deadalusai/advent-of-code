8dc28996e4821acc9e570053ad923503c55ac052
Benjamin Fox <deadalus.ai@gmail.com>
2017-12-26 10:24:30 +1300

    Day 2 Part 2

--- 2017/day2/src/main.rs
+++ 2017/day2/src/main.rs
@@ -10,10 +10,26 @@ fn main() {
                 .map(|field| field.parse::<u32>().expect("Unable to parse input field"))
                 .collect::<Vec<_>>()
         })
+        // Part 1
+        // .map(|row| {
+        //     let highest = row.iter().max().expect("Unable to find max");
+        //     let lowest = row.iter().min().expect("Unable to find min");
+        //     highest - lowest
+        // })
+        // Part 2
         .map(|row| {
-            let highest = row.iter().max().expect("Unable to find max");
-            let lowest = row.iter().min().expect("Unable to find min");
-            highest - lowest
+            // Find the two numbers in this row which divide evenly, largest over smallest
+            for (i, a) in row.iter().enumerate() {
+                for (j, b) in row.iter().enumerate() {
+                    if i == j {
+                        continue;
+                    }
+                    if a % b == 0 {
+                        return a / b;
+                    }
+                }
+            }
+            panic!("Unable to find two evenly divisible numbers in row! {:?}", row);
         })
         .sum::<u32>();
 
