6b2abf9f7453fefd157c0f225eeee8a878ddd237
Benjamin Fox <deadalus.ai@gmail.com>
2020-12-05 11:25:37 +1300

    Day 2 Part 2

--- 2020/day2/src/main.rs
+++ 2020/day2/src/main.rs
@@ -72,5 +72,35 @@ fn main() -> Result<(), AppErr> {
 
     println!("Part 1: Valid passwords: {}", valid_count);
 
+    /*
+    --- Part Two ---
+    Each policy actually describes two positions in the password, where 1 means the first
+    character, 2 means the second character, and so on. (Be careful; Toboggan Corporate
+    Policies have no concept of "index zero"!) Exactly one of these positions must
+    contain the given letter. Other occurrences of the letter are irrelevant for the
+    purposes of policy enforcement.
+
+    Given the same example list from above:
+
+        1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+        1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+        2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+    How many passwords are valid according to the new interpretation of the policies?
+    */
+
+    fn is_password_valid_v2(policy: &PasswordPolicy, password: &str) -> bool {
+        password.char_indices()
+            .filter(|(i, _)| *i == (policy.min - 1) || *i == (policy.max - 1))
+            .filter(|(_, c)| *c == policy.character)
+            .count() == 1
+    }
+
+    let valid_count = input.iter()
+        .filter(|(policy, pass)| is_password_valid_v2(policy, pass))
+        .count();
+
+    println!("Part 2: Valid passwords: {}", valid_count);
+
     Ok(())
 }
