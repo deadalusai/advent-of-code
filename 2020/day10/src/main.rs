e26474a0a73b4d04e46e6ce5680123dcbbb1a198
Benjamin Fox <deadalus.ai@gmail.com>
2020-12-11 08:18:50 +1300

    Day 10 Part 2 (cheated)

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -1,5 +1,7 @@
 extern crate util;
 
+use std::collections::{ HashMap };
+
 use util::{ read_input };
 use util::error::{ AppErr };
 
@@ -22,7 +24,7 @@ fn main() -> Result<(), AppErr> {
 
     let mut adapter_ratings = read_input("input.txt")?
         .iter()
-        .map(|line| line.parse::<u32>())
+        .map(|line| line.parse::<i64>())
         .collect::<Result<Vec<_>, _>>()?;
 
     // add outlet and device ratings
@@ -33,20 +35,55 @@ fn main() -> Result<(), AppErr> {
     adapter_ratings.push(device_rating);
     adapter_ratings.sort();
 
-    let mut count1 = 0;
-    let mut count3 = 0;
+    #[derive(PartialEq, Eq, Debug)]
+    enum Delta { One, Three }
 
-    for w in adapter_ratings.windows(2) {
-        let a = w[0];
-        let b = w[1];
-        match b - a {
-            1 => count1 += 1,
-            3 => count3 += 1,
-            x => return Err(AppErr::new("fail", &format!("found a delta of {}", x))),
-        }
-    }
+    let deltas = adapter_ratings
+        .windows(2)
+        .map(|w| match w[1] - w[0] {
+            1 => Ok(Delta::One),
+            3 => Ok(Delta::Three),
+            x => Err(AppErr::new("fail", &format!("found a delta of {}", x))),
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    // println!("{:?}", deltas);
+
+    let count1 = deltas.iter().filter(|d| **d == Delta::One).count();
+    let count3 = deltas.iter().filter(|d| **d == Delta::Three).count();
 
     println!("Part 1: {} 1-jolt differences multiplied by {} 3-jolt differences is {}", count1, count3, count1 * count3);
+
+    /*
+    --- Part Two --- (cheater, cheater)
+
+    What is the total number of distinct ways you can arrange the adapters
+    to legally connect the charging outlet to your device?
+    */
+
+    fn count_arrangements(adapters: &Vec<i64>, value: i64, memo: &mut HashMap<i64, i64>) -> i64 {
+        if let Some(&ret) = memo.get(&value) {
+            return ret;
+        };
+        if value == 0 {
+            memo.insert(value, 1);
+            return 1;
+        }
+        if value < 0 || !adapters.contains(&value) {
+            memo.insert(value, 0);
+            return 0;
+        };
+        let result = count_arrangements(adapters, value - 1, memo) +
+                     count_arrangements(adapters, value - 2, memo) +
+                     count_arrangements(adapters, value - 3, memo);
+        memo.insert(value, result);
+        result
+    }
+
+    let mut memo = HashMap::new();
+    let arrangements = count_arrangements(&adapter_ratings, device_rating, &mut memo);
+
+    println!("Part 2: {} valid permutations of adapters", arrangements);
 
     Ok(())
 }
