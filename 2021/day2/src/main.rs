51dfb8e0f0a91941064c6f7eec94bfc537369e11
Benjamin Fox <deadalus.ai@gmail.com>
2021-12-25 16:37:21 +1300

    Day 2 Part 2

--- 2021/day2/src/main.rs
+++ 2021/day2/src/main.rs
@@ -6,17 +6,14 @@ use util::error::{ AppErr };
 
 fn main() -> Result<(), AppErr> {
 
-    fn parse_isntruction(s: &str) -> Result<(String, i32), AppErr> {
+    fn parse_instruction(s: &str) -> Result<(&str, i32), AppErr> {
         let mut parts = s.split(" ");
-        Ok((parts.next().map(|s| s.to_string()).ok_or("Expected direction")?,
+        Ok((parts.next().ok_or("Expected direction")?,
             parts.next().and_then(|s| s.parse::<i32>().ok()).ok_or("Invalid distance")?))
     }
 
-    let input = 
-        read_input("input.txt")?
-            .iter()
-            .map(|s| parse_isntruction(s))
-            .collect::<Result<Vec<_>, AppErr>>()?;
+    let input = read_input("input.txt")?;
+
     /*
         --- Part One ---
         Calculate the horizontal position and depth you would have after following the planned course.
@@ -26,11 +23,48 @@ fn main() -> Result<(), AppErr> {
     let mut h = 0;
     let mut d = 0;
 
-    for (inst, dist) in input.iter() {
-        match inst.as_str() {
+    for input in input.iter() {
+        let (inst, dist) = parse_instruction(input)?;
+        match inst {
             "forward" => h += dist,
             "up"      => d -= dist,
             "down"    => d += dist,
+            _         => return Err(AppErr::from("Invalid instruction")),
+        }
+    }
+
+    let result = h * d;
+
+    println!("Part 1: {}", result);
+
+    /*
+        --- Part Two ---
+
+        In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+        - down X increases your aim by X units.
+        - up X decreases your aim by X units.
+        - forward X does two things:
+            - It increases your horizontal position by X units.
+            - It increases your depth by your aim multiplied by X.
+
+        Using this new interpretation of the commands, calculate the horizontal position and depth you would have after
+        following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+    */
+
+    let mut h = 0;
+    let mut d = 0;
+    let mut aim = 0;
+
+    for input in input.iter() {
+        let (inst, dist) = parse_instruction(input)?;
+        match inst {
+            "forward" => {
+                h += dist;
+                d += aim * dist;
+            }
+            "up"      => aim -= dist,
+            "down"    => aim += dist,
             _         => return Err(AppErr::from("Invalid instruction")),
         }
     }
