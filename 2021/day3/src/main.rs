8691c65dcdac50d968933ded75028238761a9619
Benjamin Fox <deadalus.ai@gmail.com>
2021-12-29 13:10:57 +1300

    Day 3 Part 2

--- 2021/day3/src/main.rs
+++ 2021/day3/src/main.rs
@@ -21,38 +21,88 @@ fn main() -> Result<(), AppErr> {
         Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together.
         What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
     */
-
-    let column_count = input.iter().next().unwrap().trim().len();
-    let row_count = input.len();
-
-    let mut ones_counts = vec![0; column_count];
-
-    for row in input.iter() {
-        for (i, count) in ones_counts.iter_mut().enumerate() {
-            if &row[i..=i] == "1" {
-                *count += 1;
+    
+    fn count_bits_at_index(input: &[String], at_bit_index: usize) -> (usize, usize) {
+        let mut ones_count = 0;
+        let mut zeroes_count = 0;
+        for line in input.iter() {
+            match &line[at_bit_index..=at_bit_index] {
+                "1" => ones_count += 1,
+                "0" => zeroes_count += 1,
+                _   => unreachable!(),
             }
         }
+        (ones_count, zeroes_count)
     }
+
+    let column_count = input.iter().next().unwrap().len();
 
     let mut gamma = 0;
     let mut epsilon = 0;
-    let half = row_count / 2;
 
-    for (i, &count) in ones_counts.iter().enumerate() {
+    for i in 0..column_count {
+        let (ones, zeroes) = count_bits_at_index(&input, i);
         // Note: bits in input are LEAST SIGNIFICANT first
         //  input("10110") => usize(01101)
         let bit_index = column_count - i - 1;
-        gamma   |= if count > half { 1 << bit_index } else { 0 };
-        epsilon |= if count < half { 1 << bit_index } else { 0 };
-        // println!("g: {:b}, e: {:b}", gamma, epsilon);
+        gamma   |= if ones > zeroes { 1 << bit_index } else { 0 };
+        epsilon |= if zeroes > ones { 1 << bit_index } else { 0 };
     }
 
-    println!("g: {}, e: {}", gamma, epsilon);
+    println!("g: {:b}, e: {:b}", gamma, epsilon);
 
     let result = gamma * epsilon;
-
     println!("Part 1: {}", result);
+
+    /*
+        --- Part Two ---
+        https://adventofcode.com/2021/day/3#part2
+
+        Use the binary numbers in your diagnostic report to calculate the oxygen generator rating and CO2 scrubber rating,
+        then multiply them together. What is the life support rating of the submarine?
+        (Be sure to represent your answer in decimal, not binary.)
+    */
+
+    enum FindBehavior { MostCommonPreferOnes, LeastCommonPreferZeroes }
+
+    fn find_entry_with_most_common_bits(mut input: Vec<String>, behavior: FindBehavior) -> Option<String> {
+        let column_count = input.iter().next().unwrap().trim().len();
+        for i in 0..column_count {
+            let (ones, zeroes) = count_bits_at_index(&input, i);
+            let filter_char = match behavior {
+                FindBehavior::MostCommonPreferOnes    => if ones >= zeroes { "1" } else { "0" },
+                FindBehavior::LeastCommonPreferZeroes => if zeroes <= ones { "0" } else { "1" },
+            };
+            // Filter to strings which match the character at this position
+            input.retain(|line| &line[i..=i] == filter_char);
+            // Got one? Found our result
+            if input.len() == 1 {
+                return input.into_iter().next();
+            }
+        }
+        None
+    }
+
+    fn input_binary_to_usize(line: &str) -> usize {
+        let len = line.len();
+        line.char_indices()
+            .fold(0_usize, |r, (i, c)| {
+                r | match c {
+                    '1' => 1 << len - i - 1,
+                    '0' => 0,
+                    _   => unreachable!(),
+                }
+            })
+    }
+
+    let oxy_generator_rating = find_entry_with_most_common_bits(input.clone(), FindBehavior::MostCommonPreferOnes).map(|s| input_binary_to_usize(&s)).expect("oxy");
+    let co2_scrubber_rating = find_entry_with_most_common_bits(input, FindBehavior::LeastCommonPreferZeroes).map(|s| input_binary_to_usize(&s)).expect("co2");
+
+    println!("oxy: {}, co2: {}", oxy_generator_rating, co2_scrubber_rating);
+
+    let result = oxy_generator_rating * co2_scrubber_rating;
+
+    println!("Part 2: {}", result);
 
     Ok(())
 }
