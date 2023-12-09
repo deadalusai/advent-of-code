e798163eb69f434604a64bf209c3f6614e916db9
Benjamin Fox <deadalus.ai@gmail.com>
2023-12-09 13:36:56 +1300

    Alternate parsing of input for day2

---
+++ 2023/day2_alt/src/main.rs
@@ -0,0 +1,105 @@
+extern crate util;
+
+use util::read_input;
+use util::error::AppErr;
+use util::parse::{Input, ParseResult, ParseResultEx};
+
+#[derive(Debug, Default)]
+struct GameRound {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+#[derive(Debug)]
+struct GameRecord {
+    id: i32,
+    rounds: Vec<GameRound>,
+}
+
+fn parse_game(input: Input) -> ParseResult<GameRecord> {
+    enum Term { EndScore, EndRound, EndRecord }
+
+    // E.g.
+    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    let (input, _) = input.parse_token("Game")?;
+    let (input, id) = input.parse_i32()?;
+    let (input, _) = input.parse_token(":")?;
+
+    // parse rounds
+    let mut rounds = Vec::new();
+    let mut input = input;
+    loop {
+        let mut round = GameRound::default();
+        // Parse scores
+        loop {
+            let next = input;
+            // {count} {label}
+            let (next, score) = next.parse_i32()?;
+            let (next, label) = next.parse_alpha()?;
+            match label {
+                "red"   => round.red += score,
+                "green" => round.green += score,
+                "blue"  => round.blue += score,
+                _ => {}
+            }
+            // scan for end of score, end of round or end of input
+            let (next, term) = next.parse_token(",").map_val(|_| Term::EndScore)
+                .or_try(|| next.parse_token(";").map_val(|_| Term::EndRound))
+                .or_try(|| next.parse_end().map_val(|_| Term::EndRecord))?;
+            input = next;
+            
+            match term {
+                Term::EndScore => {
+                    continue;
+                },
+                Term::EndRound => {
+                    rounds.push(round);
+                    break;
+                },
+                Term::EndRecord => {
+                    rounds.push(round);
+                    return Ok((input, GameRecord { id, rounds }))
+                },
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), AppErr> {
+    /*
+        --- Part One ---
+        The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+    */
+
+    let input = read_input("input.txt")?
+        .iter()
+        .map(|s| parse_game(Input::new(s)).map(|r| r.1))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let result: i32 = input.iter()
+        .filter(|g| g.rounds.iter().filter(|r| r.red > 12 || r.green > 13 || r.blue > 14).next().is_none())
+        .map(|g| g.id)
+        .sum();
+
+    println!("Part 1: {:?}", result);
+
+    /*
+        --- Part Two ---
+        For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+    */
+
+    let result: i32 = input.iter()
+        // Find the minimum cubes required to play each game
+        .map(|g| g.rounds.iter()
+                .map(|r| (r.red, r.green, r.blue))
+                .reduce(|(r1, g1, b1), (r2, g2, b2)| (r1.max(r2), g1.max(g2), b1.max(b2)))
+                .unwrap_or_default())
+        // Find the power for each minimum set
+        .map(|(r, g, b)| r * g * b)
+        .sum();
+
+    println!("Part 2: {:?}", result);
+
+    Ok(())
+}
